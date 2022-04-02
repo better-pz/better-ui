ea6bf1f43889e8fa90807fd1ec83634af6178bdf
pengzheng <pzeng08@163.com>
2022-04-02 20:34:45 +0800

    修改按钮

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -13,40 +13,40 @@ export enum ButtonType {
   Link = 'link'
 }
 interface BaseButtonProps {
-  className ?: string;
-  disabled ?: boolean;
-  size ?: ButtonSize;
-  btnType ?: ButtonType;
-  href?:string;
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  href?: string;
   children: React.ReactNode;
 }
-const Button :React.FC<BaseButtonProps> = (props) => {
-  const {className,disabled,size,btnType,href,children} = props
+const Button: React.FC<BaseButtonProps> = (props) => {
+  const { className, disabled, size, btnType, href, children } = props
   const classes = classNames("btn", className, {
     [`btn-${btnType}`]: btnType, // 当传入了 btnType 为 lg 时，添加类名 btn-lg
     [`btn-${size}`]: size,
     'disabled': btnType === ButtonType.Link && disabled,
   }
   )
-  if (btnType === ButtonType.Link && href ) {
+  if (btnType === ButtonType.Link && href) {
     return (
       <a href={href} className={classes}>
         {children}
       </a>
     )
-  }else {
+  } else {
     return (
-    <div>
-      <button className={classes} disabled={disabled}>
-        {children}
-      </button>
-    </div>
-  )
+      <div>
+        <button className={classes} disabled={disabled}>
+          {children}
+        </button>
+      </div>
+    )
   }
-  
+
 }
 Button.defaultProps = {
-  disabled:false,
-  btnType:ButtonType.Default
+  disabled: false,
+  btnType: ButtonType.Default
 }
 export default Button
